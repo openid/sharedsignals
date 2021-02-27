e9ca9f42a9d607bfa89d820530968a5bdba9132f
Tim Cappalli <tim@cloudauth.dev>
2021-02-27 22:42:23 +0000

    converted from mmark to kramdown

--- md-build.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-filename=$1
-BASENAME=${filename%.*}
-
-mmark $filename > $BASENAME.xml
-`which xml2rfc` --legacy --html $BASENAME.xml
-`which xml2rfc` --legacy --text $BASENAME.xml
